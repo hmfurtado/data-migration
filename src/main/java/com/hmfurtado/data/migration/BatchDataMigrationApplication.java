1c3faf0d2d9607b8901d339b137c655e8e7d5e9c
Heitor Furtado <heitormf@live.com>
2022-03-10 11:37:24 -0300

    first commit

---
+++ src/main/java/com/hmfurtado/data/migration/BatchDataMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.hmfurtado.data.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchDataMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchDataMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmfurtado/data/migration/BatchDataMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmfurtado.data.migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchDataMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
